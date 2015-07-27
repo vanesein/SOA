3b960a2ca4feed544e4d8e5fbe7f0978f59c1ebf
Alfonsina Ochoa <alfonsina.ochoa@cti.espol.edu.ec>
2015-07-26 19:07:07 -0500

    modificaciones: se agrego el for de los arreglos de algoritmos se agrego la funcionalidad de guardar los resultados en un archivo
    Signed-off-by: Alfonsina Ochoa <alfonsina.ochoa@cti.espol.edu.ec>

--- Alfo/cache-master2/cache-master/sim.py
+++ Alfo/cache-master2/cache-master/sim.py
@@ -19,12 +19,13 @@ l = { "CLOCK": CLOCK,
       "kRANDOM_LRU": kRANDOM_LRU, #  handled differently
 }
 
-ls="LRU"
+ls=["LRU", "RANDOM"]
 ss = []
-#slices = [1, 4, 6]
-#path = os.getcwd()+'/traces test/'
-slices = [1, 2, 4, 30, 120]
-path = os.getcwd()+'/traces/'
+arreglo = []
+path = os.getcwd()+'/traces test/'
+slices = [1, 2, 4, 10, 30, 120]
+#path = os.getcwd()+'/traces/'
+results_traces = os.getcwd()+'/results_traces/'
 
 def rename_files():
     p = 1        
@@ -60,84 +61,96 @@ def inicialize_cache():
             if(path_size < min_file_size):
                 min_file_size = path_size
             if(path_size > max_file_size):
-                max_file_size = path_size            
-    min_file_size = min_file_size/1024    
-    max_file_size = max_file_size/1024    
-    
+                max_file_size = path_size   
+            if (path_size > 1024):
+                min_file_size = min_file_size/1024    
+                max_file_size = max_file_size/1024    
+                
     for i in xrange(n_array):        
         ss.append(random.randint(min_file_size, max_file_size))
     ss.sort()    
-    
-    
+        
 def fillspace(s):
     return s + " " * (20 - len(s))
 
-def doitall():
-    algn = ls
-    
-    k = None
-    if algn.find("RANDOM_LRU") >= 0:
-        algm = kRANDOM_LRU
-        k = int(algn.replace("RANDOM_LRU_", ""))
-    elif algn[0] == "S" and algn.find("LRU") > 0:
-        algm = SkLRU
-        k = int(algn[1:].replace("LRU", ""))
-    elif algn not in l:
-        print "Algorithm: %s does not exist" % algn
-        exit()
-    else:
-        algm =  l[algn]
-    #ss = [3, 6, 9]
-    for i in xrange(len(ss)):    #recorre lista de tamaños de cache
-        c = int(ss[i])        
-        print " CACHE SIZE = " + str(ss[i])        
-        for j in xrange(len(slices)): #recorre arreglo de slices
-            alg = algm.alg(c, k=k)        
-            time1 = time.time()
-            lc = 0
-            for x in os.listdir(path):  #recorre la lista de los archivos de traces
-                f = open(path+x, "r")
-                lines = f.readlines()
-                alg.setup(lines)
-                num_lines = sum(1 for line in lines) #obtiene el número de líneas del archivo
-                f.close()
-                print " leyendo archivo " + str(x) + " de " + str(num_lines) + " lineas, slice k = " + str(slices[j])                               
-                contador = 0
-                for k in xrange(slices[j]): #divide el archivo en n partes                    
-                    if ((slices[j] > 0) and (slices[j] < num_lines)) :
-                        if(contador == 0):                            
-                            num = int(round(num_lines / slices[j],0))                            
-                            max_num = num
-                        else:                            
-                            max_num = max_num + num 
-                            if (k == slices[j] - 1):
-                                max_num = num_lines                        
-                        #print "contador: " + str(contador) + " max_num: " + str(max_num)                        
-                        arreglo_lineas = random.sample(range(contador, max_num), max_num-contador)
-                        lista = range(contador, max_num)
-                        random.shuffle(lista)
-                        #print "Reshuffled lista: " , lista
-                        #print arreglo_lineas                        
-                        contador = max_num
-                        for m in xrange(len(arreglo_lineas)):
-                            #print lines[arreglo_lineas[m]]
-                            line = lines[arreglo_lineas[m]]
-                            lc += 1
-                            b = line.replace("\n", "").lstrip()
-                            key = b.split(" ")[0]
-                            #print 'key del trace: '+ str(key)
-                            ret = alg.get(key)
-                            if not ret:
-                                alg.put(key, 1)
-                            
+def doitall():    
         
-            time2 = time.time()
-            diff = time2-time1
-            tp = lc / (0.0 + diff)
-            hr = alg.hitcount / (0.0 + alg.count)
-            print "hitcount: " +str(alg.hitcount)
-            print "%s %d %.4f %.2f" % (str(alg), c, 100.0*hr, tp)
-                #sys.stderr.write("%s with %d\tHit ratio: %.4f\tThroughput: %.2f (reqs/s)\n" % (fillspace(str(alg)), c, hr, tp))
+    for algn in ls:    #recorre lista de algoritmos        
+        k = None
+        if algn.find("RANDOM_LRU") >= 0:
+            algm = kRANDOM_LRU
+            k = int(algn.replace("RANDOM_LRU_", ""))
+        elif algn[0] == "S" and algn.find("LRU") > 0:
+            algm = SkLRU
+            k = int(algn[1:].replace("LRU", ""))
+        elif algn not in l:
+            print "Algorithm: %s does not exist" % algn
+            exit()
+        else:
+            algm =  l[algn]
+            
+        for j in xrange(len(slices)): #recorre arreglo de slices  
+            n_slice = slices[j]            
+            file_result = open(results_traces + str(algn) + "_K_" + str(slices[j]),"a") 
+            for i in xrange(len(ss)):    #recorre lista de tamaños de cache
+                c = int(ss[i])
+                alg = algm.alg(c, k=k)        
+                time1 = time.time()
+                lc = 0
+                print " CACHE SIZE = " + str(ss[i])        
+                
+                for x in os.listdir(path):  #recorre la lista de los archivos de traces
+                    f = open(path+x, "r")
+                    lines = f.readlines()
+                    alg.setup(lines)
+                    num_lines = sum(1 for line in lines) #obtiene el número de líneas del archivo
+                    f.close()
+                    
+                    print " leyendo archivo " + str(x) + " de " + str(num_lines) + " lineas, slice k = " + str(slices[j])   
+                    if (num_lines < slices[j]):                                        
+                        n_slice = 1
+                    contador = 0
+                    
+                    for k in xrange(n_slice): #divide el archivo en n partes
+                        
+                        if ((n_slice > 0) and (n_slice < num_lines)) :
+                            if(contador == 0):                            
+                                num = int(round(num_lines / n_slice,0))                            
+                                max_num = num
+                            else:                            
+                                max_num = max_num + num 
+                                if (k == n_slice - 1):
+                                    max_num = num_lines                        
+
+                            arreglo_lineas = random.sample(range(contador, max_num), max_num-contador)
+                            lista = range(contador, max_num)
+                            random.shuffle(lista)
+                            contador = max_num
+                            for m in xrange(len(arreglo_lineas)):
+                                line = lines[arreglo_lineas[m]]
+                                lc += 1
+                                b = line.replace("\n", "").lstrip()
+                                key = b.split(" ")[0]
+                                #print 'key del trace: '+ str(key)
+                                ret = alg.get(key)
+                                if not ret:
+                                    alg.put(key, 1)
+            
+                time2 = time.time()
+                diff = time2-time1
+                tp = 0
+                if (diff > 0):
+                    tp = lc / (0.0 + diff)
+                hr = 0
+                if(alg.count > 0):
+                    hr = alg.hitcount / (0.0 + alg.count)            
+                print "hitcount: " +str(alg.hitcount)
+                print "%s %d %.4f %.2f" % (str(alg), c, 100.0*hr, tp)
+                file_result.write(str(alg) + " " + str(c) + " " + str(100.0*hr) + " " + str(tp) + "\n")
+                arreglo.append(str(slices[j]) + "_" + str(alg) + " " + str(c) + " " + str(100.0*hr) + " " + str(tp))
+        file_result.close()
+        print arreglo
+
 
 
 if __name__ == "__main__":
